e890e7494b938bd3be57847a5ba8440f15254b05
Helton Batista <=>
2025-06-06 12:01:29 -0300

    feat(ui): build initial UI components and configure client-side routing

--- src/components/maps/index.js
+++ src/components/maps/index.js
@@ -3,10 +3,11 @@ import GoogleMapReact from "google-map-react";
 import { Marker } from "../marker";
 
 export const Maps = () => {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_KEY;
   return (
     <div className="container-map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBiZIDfKa-Ch1o6n9dkSybQkC5AKemD4sE" }}
+        bootstrapURLKeys={{ key: apiKey }}
         center={{
           lat: -4.5619444444444,
           lng: -37.77,
